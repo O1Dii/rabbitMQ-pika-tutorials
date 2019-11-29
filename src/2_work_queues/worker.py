ff9dfe8f05f8d5c05709be1b8214d46808a866f0
O1Dii <oldi730051@mail.ru>
2019-11-29 14:13:03 +0300

    Added publish_subscribe tutorial, modified work_queues

--- src/2_work_queues/worker.py
+++ src/2_work_queues/worker.py
@@ -5,16 +5,18 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 
 def callback(ch, method, properties, body):
     print(f'Received {body}')
     time.sleep(body.count(b'.'))
     print('Done')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-channel.basic_consume(queue='hello', auto_ack=True, on_message_callback=callback)
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='hello', on_message_callback=callback)
 
 print('Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
