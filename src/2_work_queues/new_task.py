0f4c13e30329316e6b40ba76e2457afc4c1d7560
O1Dii <oldi730051@mail.ru>
2019-12-02 15:39:22 +0300

    Added Readme, changed work_queues

--- src/2_work_queues/new_task.py
+++ src/2_work_queues/new_task.py
@@ -10,7 +10,7 @@ channel.queue_declare(queue='task_queue', durable=True)
 message = ' '.join(sys.argv[1:]) or 'Hello World!'
 channel.basic_publish(
     exchange='',
-    routing_key='hello',
+    routing_key='task_queue',
     body=message,
     properties=pika.BasicProperties(
         delivery_mode=2,
--- src/2_work_queues/worker.py
+++ src/2_work_queues/worker.py
@@ -16,7 +16,7 @@ def callback(ch, method, properties, body):
 
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='hello', on_message_callback=callback)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
 print('Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
